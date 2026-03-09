8eef775cf1fb49ef9a634625694204f9f63a3d81
Mrfroxi <pilutikas340@gmail.com>
2026-03-09 16:38:36 +0300

    feat:setup config variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
