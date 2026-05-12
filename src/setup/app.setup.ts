90e6d007fcdb1cfa2ea05afee19df35b9af59f0e
Mrfroxi <pilutikas340@gmail.com>
2026-05-12 21:24:14 +0300

    feat: add pipesSetup

--- src/setup/app.setup.ts
+++ src/setup/app.setup.ts
@@ -1,8 +1,11 @@
 import { INestApplication } from '@nestjs/common';
 import { globalPrefixSetup } from './global-prefix.setup';
+import { pipesSetup } from '@src/setup/pipes.setup';
 
 export function appSetup(app: INestApplication) {
   app.enableCors();
+
+  pipesSetup(app);
 
   globalPrefixSetup(app);
 }
